d1d338d75811d80934b9b86d954fa8667a6f5512
programacionparaaprender <yancel209@gmail.com>
2023-03-25 19:07:47 -0500

    agregando cambios

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.obtenerDatosDeUsuario3 = exports.obtenerDatosDeUsuario = void 0;
-const axios_1 = __importDefault(require("axios"));
-function obtenerDatosDeUsuario(usuario) {
-    const url = `https://api.github.com/users/${usuario}`;
-    return axios_1.default.get(url).then(data => data.data).
-        catch(error => error.response.data);
+const axios = require('axios');
+
+module.exports = {
+    /**
+     * Introducir el usuario de github para obtener sus datos de la API
+     * @example
+     * usuario = 'mugan86' 
+     * @param { string } usuario Usuario del que queremos los datos de la API
+     */
+    obtenerDatosDeUsuario: function ( usuario ) {
+        const url = `https://api.github.com/users/${ usuario }`;
+        return axios.get(url).then(
+            data => data.data
+        ).
+        catch( error => error.response.data);
+    },
+    obtenerDatosDeUsuario3: function ( usuario ) {
+        const url = `https://api.github.com/users/${ usuario }`;
+        axios.get(url).then(
+            data => {
+                console.log(data.data);
+            }
+        ).
+        catch( error => error.response.data);
+    },
+    obtenerDatosDeUsuario2: async function ( usuario ) {
+        const url = `https://api.github.com/users/${ usuario }`;
+        try{
+            const response = await axios.get(url);
+            return response.data;
+        }catch(error){
+            return error.response.data;
+        }
+    }
 }
-exports.obtenerDatosDeUsuario = obtenerDatosDeUsuario;
-function obtenerDatosDeUsuario3(usuario) {
-    const url = `https://api.github.com/users/${usuario}`;
-    axios_1.default.get(url).then(data => {
-        const estilos = 'background-color: #ff3300; color: white; font-size: 30px; display:block;text-align:center;text-decoration:none;';
-        console.log(data.data, estilos);
-    }).
-        catch(error => error.response.data);
-}
-exports.obtenerDatosDeUsuario3 = obtenerDatosDeUsuario3;
